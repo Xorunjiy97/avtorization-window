7d400efb393a8b4e71f9497a3be898f44b3812fc
xorunjiy97 <xorunjiy.a@gmail.com>
2020-12-02 16:23:10 +0400

    create model

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './style.less';
+import Model from './model/Model';
+import Modals from './modals/Modals';
+import './autoris-style.less';
+
+function init() {
+	const db = new Model();
+
+	window.db = db;
+	const modals = new Modals(db);
+	modals.init();
+}
+
+init();
