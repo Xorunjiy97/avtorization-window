d26cac75466024de8502e2a75ca32e3f2c38b73e
GereyevKamran <jsfront20@gmail.com>
2020-12-04 21:02:10 +0400

    some

--- src/modals/registrModalWindow/RegistrModalWindow.js
+++ src/modals/registrModalWindow/RegistrModalWindow.js
@@ -1,82 +1,82 @@
 class Registr {
-	constructor (db, autWin) {
-		this._db = db;
-		this.autoris = autWin;
-		this.newDiv = null;
-	}
+  constructor(db, autWin) {
+    this._db = db;
+    this.autoris = autWin;
+    this.newDiv = null;
+  }
 
-	getValue = event => {
-		event.preventDefault();
+  getValue = (event) => {
+    event.preventDefault();
 
-		const login = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
-		const currentValue = {
-			login,
-			password,
-		};
+    const login = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const currentValue = {
+      login,
+      password,
+    };
 
-		this._db.saveUser(currentValue);
+    this._db.check(login, password);
 
-		this.clearInput();
-		const rem = document.getElementById('registr-box');
-		const root = document.getElementById('root');
-		this.newDiv.remove(this.newDiv);
-		root.append(this.autoris.createDinamicLogIn());
-        //создать функцию которая будет переключать страницу на authPage
-	}
+    this.clearInput();
+    const rem = document.getElementById("registr-box");
+    const root = document.getElementById("root");
+    this.newDiv.remove(this.newDiv);
+    root.append(this.autoris.createDinamicLogIn());
+    //создать функцию которая будет переключать страницу на authPage
+  };
 
-    clearInput = () => {
-      document.getElementById("username").value = '';
-      document.getElementById("password").value = '';
-    }
+  clearInput = () => {
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+  };
 
-    createDinamicLogIn = () => {
-		this.newDiv = document.createElement('div');
-		this.newDiv.setAttribute('class', 'registr-box');
-		// document.getElementById('root').appendChild(newDiv);
-		
-		const newH2 = document.createElement('h2');
-		newH2.innerText = 'registration ';
-		this.newDiv.append(newH2);
+  createDinamicLogIn = () => {
+    this.newDiv = document.createElement("div");
+    this.newDiv.setAttribute("class", "registr-box");
+    // document.getElementById('root').appendChild(newDiv);
 
-		const newForm =  document.createElement('form');
-		this.newDiv.append(newForm);
+    const newH2 = document.createElement("h2");
+    newH2.innerText = "registration ";
+    this.newDiv.append(newH2);
 
-		const newDiv2 =  document.createElement('div');
-		newDiv2.setAttribute('class', 'user-box');
-		newForm.append(newDiv2);
+    const newForm = document.createElement("form");
+    this.newDiv.append(newForm);
 
-		const newInput = document.createElement('input');
-		newInput.setAttribute('id','username');
-		newDiv2.append(newInput);
+    const newDiv2 = document.createElement("div");
+    newDiv2.setAttribute("class", "user-box");
+    newForm.append(newDiv2);
 
-		const newLabel = document.createElement('label');
-		const labelTxt = document.createTextNode('Username');
-		newLabel.append(labelTxt);
-		newDiv2.append(newLabel);
+    const newInput = document.createElement("input");
+    newInput.setAttribute("id", "username");
+    newDiv2.append(newInput);
 
-		const newDiv3 = document.createElement('div');
-		newDiv3.setAttribute('class', 'user-box');
-		newForm.append(newDiv3);
+    const newLabel = document.createElement("label");
+    const labelTxt = document.createTextNode("Username");
+    newLabel.append(labelTxt);
+    newDiv2.append(newLabel);
 
-		const newInput2 = document.createElement('input');
-		newInput2.setAttribute('id','password');
-		newDiv3.append(newInput2);
+    const newDiv3 = document.createElement("div");
+    newDiv3.setAttribute("class", "user-box");
+    newForm.append(newDiv3);
 
-		const newLabel2 = document.createElement('label');
-		newLabel2.innerText = 'Password';
-		newDiv3.append(newLabel2);
+    const newInput2 = document.createElement("input");
+    newInput2.setAttribute("id", "password");
+    newDiv3.append(newInput2);
 
-		const singInButton = document.createElement('button');
-		singInButton.setAttribute('type', 'submit');
+    const newLabel2 = document.createElement("label");
+    newLabel2.innerText = "Password";
+    newDiv3.append(newLabel2);
 
-		singInButton.innerText = 'registration ';
-		singInButton.setAttribute('id', 'submit');
-		singInButton.addEventListener('click', this.getValue)
-		newForm.append(singInButton);
+    const singInButton = document.createElement("button");
+    singInButton.setAttribute("type", "submit");
 
-		return this.newDiv;
-	}
+    singInButton.innerText = "registration ";
+    singInButton.setAttribute("id", "submit");
+    singInButton.addEventListener("click", this.getValue);
+    newForm.append(singInButton);
+
+    return this.newDiv;
+  };
 }
 
 export default Registr;
